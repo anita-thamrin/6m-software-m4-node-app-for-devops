1da758a1a6d9e03fd694901a8743036a82c29f37
Anita Thamrin <anita.thamrin@gmail.com>
2025-08-29 21:42:37 +0800

    done assignment adding publish job

--- controller.test.js
+++ controller.test.js
@@ -1,19 +1,19 @@
-const targetFn = require("./controller");
+const { print } = require("./controller");
 
 describe("the print function", ()=>{
 
     // standard best practice
     beforeEach(()=>{
         jest.clearAllMocks();
-    })
+    });
 
     it("should respond with 'Hello world!'", ()=>{
         const mockReq = {};
         const mockRes = {
             send:jest.fn()
-        }
+        };
 
-        targetFn(mockReq, mockRes);
+        print(mockReq, mockRes);
         expect(mockRes.send).toHaveBeenCalledWith("Hello world!!"); // If you change this value, the test will fail.
-    })
-})
+    });
+});
